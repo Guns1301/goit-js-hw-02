212e743da8a2c6de981824dd074df13092e680db
Guns1301 <67137938+Guns1301@users.noreply.github.com>
2020-10-05 21:24:26 +0300

    module 2

---
+++ task-2_2.js
@@ -0,0 +1,23 @@
+const calculateEngravingPrice = function (message = '', pricePerWord = 0) {
+  return message.split(' ').length * pricePerWord;
+};
+
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    10,
+  ),
+);
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    20,
+  ),
+);
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
+);
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
+);
+console.log(calculateEngravingPrice('Uno', 100));
